8ab9b375147bee7fa084275fbe8021363bfb7016
Minju Kim <mindori1019@gmail.com>
2023-08-08 22:37:36 +0900

    Feature : 비만도 계산하는 첫 번째 화면 완성 (키와 몸무게 입력받아 viewModel에서 처리하게 함)

--- Calculator/app/src/main/java/com/mjkim/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/mjkim/calculator/MainActivity.kt
@@ -3,14 +3,24 @@ package com.mjkim.calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.mjkim.calculator.ui.theme.CalculatorTheme
+import com.mjkim.calculator.ui.theme.calculatorShapes
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,20 +33,49 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun MainScreen(){
+fun MainScreen() {
+    val viewModel: MainViewModel = viewModel()
+    val keyboardController = LocalSoftwareKeyboardController.current
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 20.dp, vertical = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "키")
-        Text(text = "몸무게")
+        val h = mainTextField(title = "키")
+        val w = mainTextField(title = "몸무게")
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(
+            modifier = Modifier
+                .widthIn(40.dp)
+                .heightIn(20.dp),
+            shape = calculatorShapes.small,
+            onClick = {
+                keyboardController?.hide()
+                viewModel.calculate(height = h.toDouble(), weight = w.toDouble())
+                println("${viewModel.rankSate.value}")
+            }
+        ) {
+            Text("결과")
+        }
     }
 }
 
 @Composable
-fun TextField(title: String){
-    TextField(title = title)
+fun mainTextField(title: String): String {
+    val (text, setText) = remember {
+        mutableStateOf("")
+    }
+    TextField(
+        value = text,
+        onValueChange = setText,
+        label = { Text(title) },
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next)
+    )
+    return text
 }
 
 @Preview(showBackground = true)
---
+++ Calculator/app/src/main/java/com/mjkim/calculator/MainViewModel.kt
@@ -0,0 +1,26 @@
+package com.mjkim.calculator
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    private val _rankState:MutableState<Rank> = mutableStateOf(Rank.NONE)
+    val rankSate: State<Rank> = _rankState
+
+    fun calculate(height: Double, weight: Double){
+        val bmi: Double = weight / (height * height) * 100
+        _rankState.value = when {
+            bmi <= 18.5 -> Rank.LOW
+            bmi <= 22.9 -> Rank.NORMAL
+            bmi <= 24.9 -> Rank.OVER
+            bmi >= 25 -> Rank.OBESITY
+            else -> {Rank.NONE}
+        }
+    }
+}
+
+enum class Rank(val kor: String){
+    LOW("저체중"), NORMAL("정상"), OVER("과체중"), OBESITY("비만"), NONE("측정이 잘못되었습니다")
+}
