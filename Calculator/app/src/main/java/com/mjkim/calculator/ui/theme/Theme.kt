f563191492d4c024253c380bdd6444e0f07ebf22
Minju Kim <mindori1019@gmail.com>
2023-08-08 22:01:18 +0900

    Style : shape, typography 파일 따로 두어 변수로 설정

--- Calculator/app/src/main/java/com/mjkim/calculator/ui/theme/Theme.kt
+++ Calculator/app/src/main/java/com/mjkim/calculator/ui/theme/Theme.kt
@@ -1,13 +1,8 @@
 package com.mjkim.calculator.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 private val DarkColorPalette = darkColorScheme(
     primary = dark_primary,
@@ -28,53 +23,6 @@ fun CalculatorTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Compos
     } else {
         LightColorPalette
     }
-
-    val calculatorTypography = Typography(
-        titleLarge = TextStyle(
-            fontWeight = FontWeight.SemiBold,
-            fontSize =  22.sp,
-            lineHeight = 28.sp,
-            letterSpacing = 0.sp
-        ),
-        titleMedium = TextStyle(
-            fontWeight = FontWeight.SemiBold,
-            fontSize =  16.sp,
-            lineHeight = 24.sp,
-            letterSpacing = 0.sp
-        ),
-        titleSmall = TextStyle(
-            fontWeight = FontWeight.SemiBold,
-            fontSize =  14.sp,
-            lineHeight = 20.sp,
-            letterSpacing = 0.sp
-        ),
-        bodyLarge = TextStyle(
-            fontWeight = FontWeight.Normal,
-            fontSize = 16.sp,
-            lineHeight = 24.sp,
-            letterSpacing = 0.sp,
-        ),
-        bodyMedium = TextStyle(
-            fontWeight = FontWeight.Normal,
-            fontSize = 14.sp,
-            lineHeight = 20.sp,
-            letterSpacing = 0.sp,
-        ),
-        bodySmall = TextStyle(
-            fontWeight = FontWeight.Normal,
-            fontSize = 12.sp,
-            lineHeight = 16.sp,
-            letterSpacing = 0.sp,
-        )
-    )
-
-    val calculatorShapes = Shapes(
-        extraSmall = RoundedCornerShape(4.dp),
-        small = RoundedCornerShape(8.dp),
-        medium = RoundedCornerShape(12.dp),
-        large = RoundedCornerShape(16.dp),
-        extraLarge = RoundedCornerShape(24.dp)
-    )
 
     MaterialTheme(
         colorScheme = calculatorColorScheme,
