b5a835f28c6f468562b656a69e85987e3e2f9aba
Minju Kim <mindori1019@gmail.com>
2023-08-08 16:47:24 +0900

    Build : 라이브러리 최신 버전으로 설정

--- Calculator/app/src/main/java/com/mjkim/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/mjkim/calculator/MainActivity.kt
@@ -3,11 +3,11 @@ package com.mjkim.calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mjkim.calculator.ui.theme.CalculatorTheme
@@ -17,27 +17,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen(){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "키")
+        Text(text = "몸무게")
+    }
+}
+
+@Composable
+fun TextField(title: String){
+    TextField(title = title)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CalculatorTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
